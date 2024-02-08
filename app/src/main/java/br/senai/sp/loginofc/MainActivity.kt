9b8742929b07e79926906b7d41f34c0c0f9cbdb8
Nicolas Petri <123608034+nvpetri@users.noreply.github.com>
2024-02-08 11:52:56 -0300

    Update MainActivity.kt

--- app/src/main/java/br/senai/sp/loginofc/MainActivity.kt
+++ app/src/main/java/br/senai/sp/loginofc/MainActivity.kt
@@ -3,10 +3,12 @@ package br.senai.sp.loginofc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,7 +16,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import br.senai.sp.loginofc.ui.theme.LoginOfcTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +30,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xFF6A1FB4)
                 ) {
                     TelaLogin()
                 }
@@ -36,8 +41,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TelaLogin() {
-    Column {
-     Text(text = "Login")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF6A1FB4))
+    ) {
+     Text(
+         text = "Login",
+         fontSize = 48.sp,
+         fontWeight = FontWeight.Bold
+     )
         Box{
             Column {
                 Text(text = "Email")
@@ -59,6 +72,10 @@ fun TelaLogin() {
                 }
             }
         }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Entrar")
+        }
+        Text(text = "Desenvolvido por Nicolas Vasconcelos - DSA")
     }
 }
 
